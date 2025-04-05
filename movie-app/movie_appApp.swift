e4356d04d5fa58cc0fc34f05a80e7c44035d7a16
Molnár Ferenc <nfecoka4@gmail.com>
2025-04-05 13:46:26 +0200

    Initial Commit

---
+++ movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Ferenc Molnar on 2025. 04. 05..
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
