3d3627d2f89edb0b84e170f4208e18e4bc80f5c8
Molnár Ferenc <nfecoka4@gmail.com>
2025-04-26 10:28:54 +0200

    a

---
+++ movie-app/helpers/LayoutConst.swift
@@ -0,0 +1,13 @@
+//
+//  LayoutConst.swift
+//  movie-app
+//
+//  Created by Ferenc Molnar on 2025. 04. 26..
+//
+
+enum LayoutConst {
+    static let normalPadding = 16
+    static let smallPadding = 8
+    static let largePadding = 24
+    static let maxPadding = 30
+}
